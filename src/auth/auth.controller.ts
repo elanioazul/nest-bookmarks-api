7bae35345af90c679103e48e273c01cba588f20e
hugo <hugo.herrador@gmail.com>
2022-11-09 17:25:19 +0100

    Uses Body decorator so the under the hood framework in Nest might change, but dto model will remain

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,4 @@
-import { Controller, Post, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -7,8 +6,10 @@ export class AuthController {
   constructor(private authServ: AuthService) {}
 
   @Post('signup')
-  signup(@Req() req: Request) {
-    console.log(req.body);
+  signup(@Body() dto: any) {
+    console.log({
+      dto,
+    });
     return this.authServ.signup();
   }
 

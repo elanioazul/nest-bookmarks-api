b50cce16ddab608c87522ba2c514549298bd8b51
hugo <hugo.herrador@gmail.com>
2022-11-09 17:41:57 +0100

    Makes use of its dto object

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authServ: AuthService) {}
 
   @Post('signup')
-  signup(@Body() dto: any) {
+  signup(@Body() dto: AuthDto) {
     console.log({
       dto,
     });

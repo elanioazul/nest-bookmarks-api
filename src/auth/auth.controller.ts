11110e250a64b4ea7935b77875712eb86967c07b
hugo <hugo.herrador@gmail.com>
2022-11-09 18:35:41 +0100

    Make use if Pipe to tranform argument sent in post request

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, ParseIntPipe, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
@@ -7,9 +7,15 @@ export class AuthController {
   constructor(private authServ: AuthService) {}
 
   @Post('signup')
-  signup(@Body() dto: AuthDto) {
+  signup(
+    @Body('email') email: string,
+    @Body('password', ParseIntPipe) password: string,
+  ) {
     console.log({
-      dto,
+      email,
+      typeOfEmail: typeof email,
+      password,
+      typeOfPassword: typeof password,
     });
     return this.authServ.signup();
   }

8b33144f00abc6082db296c3dac1efc4823025ba
hugo <hugo.herrador@gmail.com>
2022-11-10 18:48:11 +0100

    Release a forbiden exception in case same email is sent for another user trying to sing up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,25 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 @Injectable()
 export class AuthService {
-  constructor(private prismaServ: PrismaService) {}
-  async signup(dto: AuthDto) {
+  constructor(
+    private prismaServ: PrismaService
+  ) {}
+  async signup(
+    dto: AuthDto
+  ) {
     // generate the password hash
-    const hash = await argon.hash(dto.password);
+    const hash =
+      await argon.hash(
+        dto.password
+      );
     // save the new user in the db
-    const user = await this.prismaServ.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
-    // return the saved user
-    delete user.hash;
-    return user;
+    try {
+      const user =
+        await this.prismaServ.user.create(
+          {
+            data: {
+              email:
+                dto.email,
+              hash
+            }
+          }
+        );
+      // return the saved user
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (
+          error.code ===
+          'P2002'
+        ) {
+          throw new ForbiddenException(
+            'Credentials taken'
+          );
+        }
+      }
+      throw error;
+    }
   }
   login() {
-    return { mesg: 'I have signup' };
+    return {
+      mesg: 'I have signup'
+    };
   }
 }

7f60a244ae169e30f8181218c51cc4e9cb05c0b1
hugo <hugo.herrador@gmail.com>
2022-11-23 20:18:08 +0100

    Create custom decortator for a controller mehtod

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,14 +5,16 @@ import {
   UseGuards
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from '../auth/decorator/';
 import { Request } from 'express';
 import { JwtGuard } from 'src/auth/guard';
+import { User } from '@prisma/client';
 
 @Controller('users')
 export class UserController {
   @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: User) {
+    return user;
   }
 }

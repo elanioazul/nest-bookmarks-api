36ce55656338f4dc4699ef113158cb40bc5b6cd4
hugo <hugo.herrador@gmail.com>
2022-11-23 20:19:17 +0100

    Move guard so it can be used from the global level to all endpoints of the controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,9 +10,9 @@ import { Request } from 'express';
 import { JwtGuard } from 'src/auth/guard';
 import { User } from '@prisma/client';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  @UseGuards(JwtGuard)
   @Get('me')
   getMe(@GetUser() user: User) {
     return user;
